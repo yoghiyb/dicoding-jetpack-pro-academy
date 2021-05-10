4bd4db7d4b491a7e299aab177189ccd0b669f302
yoghiyb <yoghiyb@gmail.com>
2021-05-10 09:12:31 +0700

    init from mac

---
+++ app/src/main/java/com/example/academy/ui/reader/CourseReaderActivity.kt
@@ -0,0 +1,17 @@
+package com.example.academy.ui.reader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.academy.R
+
+class CourseReaderActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_COURSE_ID = "extra_course_id"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_reader)
+    }
+}
